ac336a7fff80ad645a585958d1231b6798ed4eb1
Eduardo Apolonio dos Santos <112430322+SrDudu666@users.noreply.github.com>
2024-04-03 10:08:55 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,33 @@
+/**
+ * O algoritmo ifelse serve para validação de dados, por exemplo:
+ * if (variavel1 == variavel2) {algo irá aconteecer se a variavel1 for igual a variavel2} else {algo irá acontecer se a variavel1 for diferente da variavel2}
+ * 
+ * if: nome do algoritmo
+ * parênteses (): local onde se declaram os parâmetros ou expressão a ser validada
+ * chaves {}: bloco de códigos a serem executados, ou seja, tudo que estiver dentro do bloco de códigos será executado
+ * else: "senão" é onde serão executados os códigos, que estiverem dentro do bloco {}, caso a validação do algoritmo "if" retorne falsa
+ * 
+ * operadores lógicos:
+ * 
+ * == serve para comparar se uma variavel/valor é igual a outra
+ * != serve para comparar se uma variavel/valor é diferente de outra
+ * > serve para comparar se uma variavel/valor é maior que a outra
+ * < serve para comparar se uma variavel/valor é menor que a outra
+ * >= serve para comparar se uma variavel/valor é maior ou igual a outra
+ * <= serve para comparar se uma variavel/valor é menor ou igual a outra
+ * 
+ * se "falso": adicionar o ponto de exclamação no início da declaração de parâmetros/expressão, por exemplo:
+ * if(!variavel == variavel2) { bloco de código} aqui significa que: se a comparação da variavel1 com a variavel2 retornar falso, os códigos que estiverem dentro do bloco de código serão
+ * executados, não havendo a necessidade do uso da cláusula "else"
+ * 
+ * criar um validador onde você irá declarar duas variaveis, cada uma com um valor DIFERENTE da outra, então haverá uma resposta diferente para cada situação 
+ * 
+ * vamos criar dois inputs, onde cada input irá para cada uma das duas variaveis, ou seja:
+ * <input type="text" id="txtVar1" />
+ * <input type="text" id="txtVar2" />
+ * 
+ * var txtVar1.value que tem que ser comparado com o txtVar2.value
+ * 
+ * enviar os dados via console.log()
+ */
+
